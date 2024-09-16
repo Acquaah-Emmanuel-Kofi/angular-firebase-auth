414a1bbcdb16400227f2ee093959282b05ffbffa
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-16 21:21:54 +0000

    feat: implement toaster ui with dynamic data

--- src/app/shared/components/toaster/toaster.component.ts
+++ src/app/shared/components/toaster/toaster.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toaster',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './toaster.component.html',
-  styleUrl: './toaster.component.scss'
+  styleUrl: './toaster.component.scss',
 })
-export class ToasterComponent {
+export class ToasterComponent implements OnInit {
+  message = input.required<string>();
+  @Input() show: boolean = false;
+  status = input.required<'success' | 'error' | 'info'>();
 
+  ngOnInit(): void {
+    if (this.show) {
+      setTimeout(() => {
+        this.show = false;
+      }, 3000);
+    }
+  }
 }
