0028d8137aabce71dc301556c48a421d1dffce75
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-16 10:03:11 +0000

    feat: add dynamic input props for brand

--- src/app/shared/components/brand/brand.component.ts
+++ src/app/shared/components/brand/brand.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-brand',
   standalone: true,
   imports: [],
   templateUrl: './brand.component.html',
-  styleUrl: './brand.component.scss'
+  styleUrl: './brand.component.scss',
 })
 export class BrandComponent {
-
+  size = input<number>(113);
+  color = input<string>('');
 }
