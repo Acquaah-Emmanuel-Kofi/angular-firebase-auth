cc87886448c186e0b887f6401b4a9cf6eda94b72
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-16 16:46:40 +0000

    feat: implement functionality to create a user

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  UserCredential,
+} from '@angular/fire/auth';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private _fireAuth = inject(Auth);
 
-  constructor() { }
+  constructor() {}
+
+  signUp(email: string, password: string): Observable<UserCredential> {
+    const response = createUserWithEmailAndPassword(
+      this._fireAuth,
+      email,
+      password
+    )
+      .then(async (response) => {
+        await sendEmailVerification(response.user);
+
+        return response;
+      })
+      .catch((error) => {
+        throw new Error(error.message);
+      });
+
+    return from(response);
+  }
 }
