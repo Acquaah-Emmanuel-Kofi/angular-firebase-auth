a6def933069befaf30a22ef5b2ea8b9213775da1
waglarry <emmanuelacquaah2468@gmail.com>
2024-09-16 17:28:00 +0000

    feat: implement logout on dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  private _authService = inject(AuthService);
 
+  logout() {
+    this._authService.signOut();
+  }
 }
